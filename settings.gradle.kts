1f9a0d97908af5339802db29fcbed08f812a6476
Mahbubur Rahman <rifat0153@gmail.com>
2021-12-29 21:42:59 +0000

    Project initial setup complete

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.rifat.boruto-ktor-api"
---
+++ src/main/kotlin/com/rifat/Application.kt
@@ -0,0 +1,14 @@
+package com.rifat
+
+import io.ktor.application.*
+import com.rifat.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+    configureMonitoring()
+}
